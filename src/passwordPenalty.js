cc048d69599bf6585a156873163b940710f961c3
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-10-09 16:40:12 +0200

    The password validation exercises

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -7,10 +7,28 @@
  */
 export default function penaltyPoints(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
-  if (typeof password !== "string") password = String(password);
+  if (password !== null){
+    if (typeof password !== "string") password = String(password);
+    // const regex = /(.)\1+/gi;
+    // const sequence = password.match(regex)
+    let points = 0;
 
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  // * * * INSERT YOUR CODE HERE * * * * * * * * * * * * * *
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  //
+    const regex = /(\w)\1+/g;
+    const sequences = password.match(regex);
+
+    if (sequences) {
+      for (const sequence of sequences) {
+        if (sequence.length === 2) {
+          points += 1;
+        } else if (sequence.length >= 3) {
+          points += 2;
+        }
+      }
+      return points;
+    } else {
+      return 0
+    }
+
+  }
+  return 0
 }
--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -1,26 +1,50 @@
 export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 
-/**
- * Checks if a given password is valid or invalid.
- * If valid it returns true, otherwise false
- * @param {string} password
- * @returns {boolean}
- */
 export default function isValidPassword(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
 
-  // * * * YOUR CODE GOES IN HERE ... * * *
-  /*
-   * if (password is not exactly 10 digits or characters...) {
-   *   return ...;
-   * }
-   *
-   * if (is not composed by digits and numbers) {
-   *   return ...;
-   * }
-   */
-  const setOfPassword = new Set([...password]);
-  if (setOfPassword.size < 4) return false;
+  if (password.length !== 10) {
+    return false;
+  }
+
+  
+  if (!/^[a-zA-Z0-9]+$/.test(password)) {
+    return false;
+  }
+
+  for (let i = 0; i < password.length - 2; i++) {
+    const char1 = password.charCodeAt(i);
+    const char2 = password.charCodeAt(i + 1);
+    const char3 = password.charCodeAt(i + 2);
+
+    if (
+      (char2 === char1 + 1 && char3 === char2 + 1) ||
+      (char2 === char1 - 1 && char3 === char2 - 1)
+    ) {
+      return false;
+    }
+  }
+
+  
+  if (forbiddenPasswords.includes(password)) {
+    return false;
+  }
+
+  
+  const uniqueChars = new Set(password);
+  if (uniqueChars.size < 4) {
+    return false;
+  }
+
+
+  if (!/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
+    return false;
+  }
+
+  
+  if (password.replace(/[^0-9]/g, "").length < 2) {
+    return false;
+  }
+
   return true;
 }
